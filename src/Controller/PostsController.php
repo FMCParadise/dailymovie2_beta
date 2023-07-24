d273bbc60a318c6c9a94cfe3547772e699017817
mendev200 <irca.mendev@gmail.com>
2023-07-24 14:09:51 +0200

    ajout finBySlug

--- src/Controller/PostsController.php
+++ src/Controller/PostsController.php
@@ -1,18 +1,25 @@
 <?php
 
+// src/Controller/PostsController.php
+
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Post;
+use App\Repository\PostsRepository;
 
 class PostsController extends AbstractController
 {
-    #[Route('/posts', name: 'app_posts')]
-    public function index(): Response
+    #[Route('/posts/{slug}', name: 'app_posts')]
+    public function index(PostsRepository $posts): Response
     {
+
+        $post = $posts->findBySlug('uncharted-film-6');
+
         return $this->render('posts/index.html.twig', [
-            'controller_name' => 'PostsController',
+            'post' => $post,
         ]);
     }
 }
