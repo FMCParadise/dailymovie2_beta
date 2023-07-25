65a17803b3804cebcdf02fbfa21320acbc6b8b6c
= <=>
2023-07-25 16:28:59 +0200

    add gestionsController with pagination and edit methode with firewall

--- src/Controller/GestionsController.php
+++ src/Controller/GestionsController.php
@@ -2,22 +2,66 @@
 
 namespace App\Controller;
 
+use App\Entity\Categories;
+use App\Entity\Posts;
+use App\Repository\CategoriesRepository;
+use App\Repository\PostsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Security;
+
 
 class GestionsController extends AbstractController
 {
-    #[Route('/gestions', name: 'app_gestions')]
-    public function index(): Response
+
+    #[Route('/gestions/{nPage?}', name: 'app_gestions', requirements: ["nPage" => "\d*"])]
+    public function index(PostsRepository $postsRepository, Security $security, Request $request): Response
     {
+
+        $title = "Liste des articles de tous les Authors";
+
+        $user = $security->getUser();
+        $criteria = [];
+        //if user is'nt admin
+        if (!in_array('ROLE_ADMIN', $user->getRoles())) {
+            $criteria['user'] = $user;
+            $title = "Liste de vos articles";
+        }
+        $limit = 8; // post par page
+        $nPage = $request->get('nPage', 1);
+        $nPage = max($nPage, 1);
+        $offset = ($nPage - 1) * $limit;
+        $posts = $postsRepository->findBy($criteria, ["id" => "DESC"], $limit, $offset);
+        $totalPosts = $postsRepository->count($criteria); // obtenir le nombre total de posts spécifiques à l'utilisateur
+        $totalPages = ceil($totalPosts / $limit); // calculer le nombre total de pages
+
+        // Générer la pagination
+        $pagination = range(1, $totalPages);
+
         return $this->render('gestions/index.html.twig', [
-            'controller_name' => 'GestionsController',
+            'posts' => $posts,
+            'title' => $title,
+            'nPage' => $nPage,
+            'pagination' => $pagination,
         ]);
     }
 
-    #[Route('/gestions/edit/{slug}' , name:'app_edit_post')]
-    public function edit() : Response {
 
+    #[Route('/gestions/edit/{slug}', name: 'app_edit_post')]
+    public function edit(Posts $posts, Security $security): Response
+    {
+        // récupérer l'utilisateur actuellement connecté
+        $user = $security->getUser();
+
+        if (!$user || ($posts->getUserId()->getId() !== $user->getId() && !in_array('ROLE_ADMIN', $user->getRoles()))) {
+            $this->addFlash('access_denied', "Accès refusé");
+            return $this->render('gestions/edit.html.twig', ['post' => []]);
+        }
+
+        return $this->render('gestions/edit.html.twig', [
+            'post' => $posts
+        ]);
     }
 }
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -13,11 +13,15 @@ use Symfony\Component\HttpFoundation\Request;
 class HomeController extends AbstractController
 {
     #[Route('/', name: '_app_home')]
-    #[Route('/home/{offset?}', name: 'app_home', requirements: ["offset" => "\d*"])]
+    #[Route('/home/{nPage?}', name: 'app_home', requirements: ["nPage" => "\d*"])]
     public function index(PostsRepository $postsRepository, Request $request): Response
     {
-        $offset = $request->get('offset', 0);
-        $limit = 7; // posts par page
+
+        $limit = 7; // post par page
+        $nPage = $request->get('nPage', 1);
+        $nPage = max($nPage, 1);
+        $offset = ($nPage - 1) * $limit;
+
         $posts = $postsRepository->findBy([], ["id" => "DESC"], $limit, $offset);
 
         $totalPosts = $postsRepository->count([]); // obtenir le nombre total de posts
@@ -28,7 +32,7 @@ class HomeController extends AbstractController
 
         return $this->render('home/index.html.twig', [
             'posts' => $posts,
-            'nPage' => $offset === 0 ? 1 : $offset,
+            'nPage' => $nPage,
             'pagination' => $pagination,
         ]);
     }
