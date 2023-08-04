f7dc9fb0f5ef67cad19ba4239666a1cc55aecb37
= <=>
2023-08-04 16:28:58 +0200

    add categoriestype

--- src/Controller/CategoriesController.php
+++ src/Controller/CategoriesController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use App\Entity\Categories;
 
 use App\Entity\Posts;
+use App\Form\CategoriesType;
+use App\Form\PostsType;
 use App\Repository\CategoriesRepository;
 use App\Repository\PostsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -14,8 +16,8 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class CategoriesController extends AbstractController
 {
-    #[Route('/categories/{slug}/{nPage?}', name: 'app_categories' ,requirements: ["nPage" => "\d*"]) ]
-    public function index(Categories $categories, PostsRepository  $postsRepository, Request $request ): Response
+    #[Route('/categories/{slug}/{nPage?}', name: 'app_categories', requirements: ["nPage" => "\d*"])]
+    public function index(Categories $categories, PostsRepository $postsRepository, Request $request): Response
     {
         $limit = 4; // post par page
         $nPage = $request->get('nPage', 1);
@@ -23,13 +25,28 @@ class CategoriesController extends AbstractController
         $offset = ($nPage - 1) * $limit;
 
         $posts = $postsRepository->getPostsByCategories($categories);
-      // dd($posts);
+        // dd($posts);
         return $this->render('categories/index.html.twig', [
             'posts' => $posts,
             'nPage' => $nPage,
-            'slug' => $categories-> getSlug(),
+            'slug' => $categories->getSlug(),
             'pagination' => [],
 
         ]);
     }
+
+    #[Route("/gestions/add-categories", name: "app_add_categories")]
+    public function add(Request $request): Response
+    {
+        $categories = new Categories();
+        $form = $this->createForm(CategoriesType::class, $categories);
+        $form->handleRequest($request);
+
+
+        return $this->render('gestions/add-categories.html.twig', [
+            "title" => "add categorie",
+            'form' => $form->createView(),
+        ]);
+    }
+
 }
