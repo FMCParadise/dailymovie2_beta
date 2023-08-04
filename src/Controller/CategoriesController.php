835da9a3092de76d600dd7cdc87c4812bdded58d
Corentin Chomet <fmcp4radise@gmail.com>
2023-08-04 08:39:28 +0200

    Recuperation fichers

--- src/Controller/CategoriesController.php
+++ src/Controller/CategoriesController.php
@@ -3,33 +3,45 @@
 namespace App\Controller;
 
 use App\Entity\Categories;
-
 use App\Entity\Posts;
 use App\Repository\CategoriesRepository;
 use App\Repository\PostsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class CategoriesController extends AbstractController
 {
-    #[Route('/categories/{slug}/{nPage?}', name: 'app_categories' ,requirements: ["nPage" => "\d*"]) ]
-    public function index(Categories $categories, PostsRepository  $postsRepository, Request $request ): Response
+    #[Route('/categories/{slug}/{nPage?}', name: 'app_categories', requirements: ["nPage" => "\d*"])]
+    public function index(Categories $categories, PostsRepository  $postsRepository, Request $request): Response
     {
         $limit = 4; // post par page
         $nPage = $request->get('nPage', 1);
         $nPage = max($nPage, 1);
         $offset = ($nPage - 1) * $limit;
 
-        $posts = $postsRepository->getPostsByCategories($categories);
-      // dd($posts);
+
+
+        $posts = $postsRepository->getPostsByCategories($categories, $limit, $offset);
+
+        $totalPosts = $posts["total"]; // obtenir le nombre total de posts
+
+
+        $totalPages = ceil($totalPosts / $limit); // calculate total pages
+
+        // Générer la pagination
+        $pagination = range(1, $totalPages);
+
+
+        // dd($posts);
         return $this->render('categories/index.html.twig', [
-            'posts' => $posts,
+            'posts' => $posts["data"],
             'nPage' => $nPage,
-            'slug' => $categories-> getSlug(),
-            'pagination' => [],
+            'slug' => $categories->getSlug(),
+            'pagination' => $pagination,
 
         ]);
     }
 }
+
