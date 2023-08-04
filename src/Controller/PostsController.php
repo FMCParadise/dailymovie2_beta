031f8e2d47494452af0882fd9971001bfd02eb6c
mendev200 <irca.mendev@gmail.com>
2023-08-04 16:49:42 +0200

    add posts user

--- src/Controller/PostsController.php
+++ src/Controller/PostsController.php
@@ -29,64 +29,72 @@ class PostsController extends AbstractController
     }
 
 
-    #[Route("/posts/user/{userId}", name: 'app_posts_user')]
-    public function postsByUser(int $userId, ManagerRegistry $registry): Response
+    #[Route("/posts/user/{id}", name: 'app_posts_user')]
+    public function postsByUser(User $user, PostsRepository $postsRepository, Request $request): Response
     {
-        $user = $registry->getRepository(User::class)->find($userId);
 
-        if (!$user) {
-            throw $this->createNotFoundException('Auteur non trouvé pour l\'ID ' . $userId);
-        }
+        //$posts = $postsRepository->findByUser($userId);
+        $limit = 8; // post par page
+        $nPage = $request->get('nPage', 1);
+        $nPage = max($nPage, 1);
+        $offset = ($nPage - 1) * $limit;
 
-        $posts = $registry->getRepository(Posts::class)->findBy(['author' => $user]);
+        $posts = $postsRepository->findBy([
+            "user" => $user
+        ], ["id" => "DESC"], $limit, $offset);
 
-        return $this->render('posts_by_user.html.twig', [
-            'user' => $user,
+        $totalPosts = $postsRepository->count(["user" => $user]); // obtenir le nombre total de posts
+        $totalPages = ceil($totalPosts / $limit); // calculer le nombre total de pages
+
+        // Générer la pagination
+        $pagination = range(1, $totalPages);
+        return $this->render('posts/posts.html.twig', [
             'posts' => $posts,
+            'nameUser' => $user->getName(),
+            'nPage' => $nPage,
+            'pagination' => $pagination,
         ]);
     }
 
-
     #[Route('gestions/add', name: 'app_add_post')]
-    public function add(Request                $request,
-                        PostFilesService       $postFileService,
-                        Security $security,
-                        EntityManagerInterface $em): Response
-    {
+    public function add(
+        Request                $request,
+        PostFilesService       $postFileService,
+        Security $security,
+        EntityManagerInterface $em
+    ): Response {
         $post = new Posts();
         $form = $this->createForm(PostsType::class, $post);
         $form->handleRequest($request);
 
 
         if ($form->isSubmitted() && $form->isValid()) {
-                // Get the currently  user
-                $post->setUserId($security->getUser()) ;
+            // Get the currently  user
+            $post->setUserId($security->getUser());
 
-                try {
-                    // processing the image
-                    $imageFile = $form->get('image')->getData();
-                    //crop adn save image
-                    $filename = $postFileService->processFile($imageFile);
-                    $post->setImage($filename);
+            try {
+                // processing the image
+                $imageFile = $form->get('image')->getData();
+                //crop adn save image
+                $filename = $postFileService->processFile($imageFile);
+                $post->setImage($filename);
 
-                    //insert in db
-                    $em->persist($post);
-                    $em->flush();
-                    //redirect to
-                    return $this->redirectToRoute('app_gestions');
-
-                } catch (FileException $e) {
-                    $form['image']->addError(new FormError($e->getMessage()));
-                }
-
+                //insert in db
+                $em->persist($post);
+                $em->flush();
+                //redirect to
+                return $this->redirectToRoute('app_gestions');
+            } catch (FileException $e) {
+                $form['image']->addError(new FormError($e->getMessage()));
+            }
         }
 
 
-        return $this->render('gestions/add.html.twig',
+        return $this->render(
+            'gestions/add.html.twig',
             [
                 'form' => $form->createView(),
-            ]);
+            ]
+        );
     }
-
-
 }
