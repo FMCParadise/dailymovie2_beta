9bbb9686c4efde649319ad4075b4674b9a311bd9
= <=>
2023-08-07 14:55:05 +0200

    add categorie formtype , and genearte avatard to user

---
+++ src/Form/DeleteCategoriesType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Categories;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Count;
+
+class DeleteCategorieType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('id', EntityType::class, [
+                'class' => Categories::class,
+                'choice_label' => "name",
+                "expanded" => true,
+                "multiple" => true,
+                'constraints' => [
+                    new Count([
+                        'min' => 1,
+                        'minMessage' => 'Vous devez sélectionner au moins une catégorie.',
+                    ]),
+                ],
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Categories::class,
+        ]);
+    }
+}
